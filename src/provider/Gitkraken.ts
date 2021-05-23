7fc0841eae48103346a080b470e05e9c914ac0a7
wdhodges <w.d.hodges274@gmail.com>
2021-05-23 23:55:45 +0000

    boards are complete

--- src/provider/Gitkraken.ts
+++ src/provider/Gitkraken.ts
@@ -1,5 +1,7 @@
+import { EmbedAuthor } from "../model/EmbedAuthor";
 import { Embed } from "../model/Embed";
 import { BaseProvider } from "./BaseProvider";
+import { EmbedField } from '../model/EmbedField';
 
 class Gitkraken extends BaseProvider {
   private embed: Embed;
@@ -27,13 +29,13 @@ class Gitkraken extends BaseProvider {
   }
 
   constructor() {
-    super();
+    super();    
     this.setEmbedColor(0x205081);
     this.embed = new Embed();
   }
 
   public getName(): string {
-    return "Gitkraken Boards";
+    return "Gitkraken";
   }
 
   public getType(): string {
@@ -41,12 +43,91 @@ class Gitkraken extends BaseProvider {
   }
 
   public async boards(): Promise<void> {
-    // Parse the action, which for Gitkraken, is the defining factor.
-
-    console.log(this.body);
+    switch(this.body.action) {
+      case "archived":
+        this.archived();
+      case "unarchived":
+        this.unarchived();
+      case "updated":
+        this.updated();
+      case "deleted":
+        this.deleted();
+      case "labels_updated": 
+        this.labelsUpdated();
+      case "members_updated":
+        this.membersUpdated();
+      default:
+        return null;
+    }
   }
 
-  private async archived(): Promise<void> {}
+  public async columns(): Promise<void> {
+    switch(this.body.action) {
+      default:
+        return null;
+    }
+  }
+
+  private async archived(): Promise<void> {
+    this.embed.author = this.extractAuthor();
+    this.embed.title = `[${this.body.board.name}] has been archived.`
+    this.addEmbed(this.embed);
+  }
+
+  private async unarchived(): Promise<void> {
+    this.embed.author = this.extractAuthor();
+    this.embed.title = `[${this.body.board.name}] has been unarchived.`
+    this.addEmbed(this.embed);
+  }
+
+  private async updated(): Promise<void> {
+    this.embed.author = this.extractAuthor();
+    this.embed.title = `[${this.body.board.name}] has been updated.`
+    
+    const previousFeild = new EmbedField();
+    previousFeild.name = "Old";
+    previousFeild.value = this.body.board.previous.name;
+    previousFeild.inline = true;
+
+    const newField = new EmbedField();
+    newField.name = "New";
+    newField.value = this.body.board.name;
+    newField.inline = true;
+
+    this.embed.fields = [previousFeild, newField];
+    this.addEmbed(this.embed);
+  }
+
+  private async deleted(): Promise<void> {
+    this.embed.author = this.extractAuthor();
+    this.embed.title = `[${this.body.board.name}] has been deleted.`
+
+    this.embed.description = `${this.body.board.name} has been deleted. It had ${this.body.board.members.length} member(s), 
+      with ${this.body.board.invited_members.length} invited member(s).`;
+
+    this.addEmbed(this.embed);
+  }
+
+  private async labelsUpdated(): Promise<void> {
+    this.embed.author = this.extractAuthor();
+    this.embed.title = `[${this.body.board.name}] labels have been updated.`
+    
+    this.addEmbed(this.embed);
+  }
+
+  private async membersUpdated(): Promise<void> {
+    this.embed.author = this.extractAuthor();
+    this.embed.title = `[${this.body.board.name}] members have been updated.`
+
+    this.addEmbed(this.embed);
+  }
+
+  private extractAuthor(): EmbedAuthor {
+    const author = new EmbedAuthor();
+    author.name = this.body.sender.name;
+    author.icon_url = "https://www.gitkraken.com/downloads/brand-assets/gitkraken-logo-dark-sq.png"
+    return author;
+  }
 }
 
 export { Gitkraken };
--- test/gitkraken/gitkraken-spec.ts
+++ test/gitkraken/gitkraken-spec.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import { Gitkraken } from "../../src/provider/Gitkraken";
 import { Tester } from "../Tester";
 
-describe("/POST gitkraken", () => {
+describe("/POST gitkraken boards", () => {
   it("archived", async () => {
     const headers = {
       "x-gk-event": "boards"
@@ -15,4 +15,20 @@ describe("/POST gitkraken", () => {
     );
     expect(res.embeds).to.be.an("array").that.has.length(1);
   });
+
+  it('unarchived', async() => {
+    const headers = {
+      "x-gk-event": "boards"
+    };
+
+    const res = await Tester.test(
+      new Gitkraken(),
+      "gitkraken-unarchived.json",
+      headers
+    );
+
+    expect(res.embeds).to.be.an("array").that.has.length(1);
+    expect(res.embeds[0].author.name).equals('Keif');
+    expect(res.embeds[0].title).equals('[Welcome Board] has been unarchived.');
+  })
 });
